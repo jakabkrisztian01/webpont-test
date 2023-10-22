8da4c931c40d0c6f6673edfc9468c9890568ec49
Jakab Krisztián <jakab.krisztian@pepita.hu>
2023-10-22 14:00:17 +0200

    setup models

--- app/Models/City.php
+++ app/Models/City.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property string $name
+ * @property float $latitude
+ * @property float $longitude
+ * @property string $created_at
+ * @property string $updated_at
+ */
 class City extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'name',
+        'latitude',
+        'longitude',
+    ];
+
 }
--- app/Models/Weather.php
+++ app/Models/Weather.php
@@ -2,10 +2,39 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @property int $id
+ * @property int $city_id
+ * @property float $latitude
+ * @property float $longitude
+ * @property float $temperature
+ * @property int $pressure
+ * @property int $humidity
+ * @property float $min_temperature
+ * @property float $max_temperature
+ * @property string $created_at
+ * @property string $updated_at
+ *
+ * @property City $city
+ */
 class Weather extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'city_id',
+        'latitude',
+        'longitude',
+        'temperature',
+        'pressure',
+        'humidity',
+        'min_temperature',
+        'max_temperature',
+    ];
+
+    public function city(): BelongsTo
+    {
+        return $this->belongsTo(City::class);
+    }
 }
